39032482b7639a8c0049d27e6201fe87a02f3677
Saaketh Vedantam <saakethv@dhcp-10-29-101-234.dyn.mit.edu>
2023-05-11 14:56:10 -0400

    parse

--- env_tools/parse_actions.py
+++ env_tools/parse_actions.py
@@ -3,3 +3,8 @@ def parse_actions(file_name):
     with open(file_name, "r") as file:
         for line in file:
             actions.append(line.split()[2])
+    return actions
+
+if __name__ == '__main__':
+    file_name = 'file.txt'
+    print(parse_actions(file_name))
