ec0cb45edfd12466d003fda3f442da7e358f8205
Saaketh Vedantam <saakethv@dhcp-10-29-101-234.dyn.MIT.EDU>
2023-05-11 03:32:29 -0400

    add eval

---
+++ env_tools/ppo_eval.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+import matplotlib.pyplot as plt
+
+# read tf log file
+def read_tf_log(log_dir):
+    log_dir = Path(log_dir)
+    log_files = list(log_dir.glob(f'**/events.*'))
+    if len(log_files) < 1:
+        return None
+    log_file = log_files[0]
+    event_acc = EventAccumulator(log_file.as_posix())
+    event_acc.Reload()
+    tags = event_acc.Tags()
+    # scalar_success = event_acc.Scalars('train/episode_success')
+    scalar_return = event_acc.Scalars('train/episode_return/mean')
+    # success_rate = [x.value for x in scalar_success]
+    steps = [x.step for x in scalar_return]
+    returns = [x.value for x in scalar_return]
+    return steps, returns
+
+if __name__ == '__main__':
+    save_dir = "env_tools/data/bc"
+    steps, returns = read_tf_log(save_dir)
+    plt.plot(steps, returns)
+    plt.show()
